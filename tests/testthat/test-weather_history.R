6e48df00361f61ace1f2c2e413daa00752176f02
Tom Pisel <mail@tompisel.com>
2023-03-09 18:02:44 +1100

    cran skip problematic tests

--- tests/testthat/test-weather_history.R
+++ tests/testthat/test-weather_history.R
@@ -1,6 +1,9 @@
 with_mock_api({
   test_that("need to provide weather variables", {
-    expect_error(weather_history(c(45.46427, 9.18951),"2020-01-01","2020-01-03"),
+    skip_on_cran()
+    expect_error(weather_history(c(45.46427, 9.18951),
+                                 "2020-01-01",
+                                 "2020-01-03"),
                  "hourly or daily measure not supplied")
   })
 })
